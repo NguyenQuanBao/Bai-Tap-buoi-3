cb9ce0e198e63d19fe2941a71d5400b3398ac33c
nguyenquanbao <gabynexo221@gmail.com>
2022-11-25 15:12:34 +0700

    up bai

---
+++ main.js
@@ -0,0 +1,12 @@
+var a=null
+var Pi=3.141592654
+var c=null
+let R= prompt("Bans kinhs hinhf tronf")
+function HienThi() {
+    let C=2*Pi*R;
+    document.write("chu vi: "+C);
+}
+function show() {
+    let S=Pi*R^2;
+document.write("Dien tich: "+S);
+}
